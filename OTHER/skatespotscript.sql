2d2795ebfa17cd2a24f9b76929c36e61c52c3806
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2023-05-12 16:41:37 +0200

    init sql script

--- OTHER/skatespotscript.sql
+++ OTHER/skatespotscript.sql
@@ -31,11 +31,9 @@ CREATE TABLE IF NOT EXISTS `comment` (
   KEY `IDX_9474526C719C5F2F` (`spot_concerned_id`),
   CONSTRAINT `FK_9474526C719C5F2F` FOREIGN KEY (`spot_concerned_id`) REFERENCES `spot` (`id`),
   CONSTRAINT `FK_9474526CF675F31B` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table skatespot.comment : ~1 rows (environ)
-INSERT INTO `comment` (`id`, `author_id`, `spot_concerned_id`, `date`, `content`) VALUES
-	(1, 1, 1, '2023-05-10 09:07:18', 'Trop bien ce spot, il y a toujours beaucoup de skaters et les curb sont bien lisses.');
+-- Listage des données de la table skatespot.comment : ~0 rows (environ)
 
 -- Listage de la structure de table skatespot. doctrine_migration_versions
 CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
@@ -47,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
 
 -- Listage des données de la table skatespot.doctrine_migration_versions : ~1 rows (environ)
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-	('DoctrineMigrations\\Version20230509162006', '2023-05-10 08:55:55', 599);
+	('DoctrineMigrations\\Version20230509162006', '2023-05-12 06:37:24', 590);
 
 -- Listage de la structure de table skatespot. messenger_messages
 CREATE TABLE IF NOT EXISTS `messenger_messages` (
@@ -62,24 +60,24 @@ CREATE TABLE IF NOT EXISTS `messenger_messages` (
   KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
   KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
   KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table skatespot.messenger_messages : ~0 rows (environ)
+-- Listage des données de la table skatespot.messenger_messages : ~1 rows (environ)
+INSERT INTO `messenger_messages` (`id`, `body`, `headers`, `queue_name`, `created_at`, `available_at`, `delivered_at`) VALUES
+	(1, 'O:36:\\"Symfony\\\\Component\\\\Messenger\\\\Envelope\\":2:{s:44:\\"\\0Symfony\\\\Component\\\\Messenger\\\\Envelope\\0stamps\\";a:1:{s:46:\\"Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\BusNameStamp\\";a:1:{i:0;O:46:\\"Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\BusNameStamp\\":1:{s:55:\\"\\0Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\BusNameStamp\\0busName\\";s:21:\\"messenger.bus.default\\";}}}s:45:\\"\\0Symfony\\\\Component\\\\Messenger\\\\Envelope\\0message\\";O:51:\\"Symfony\\\\Component\\\\Mailer\\\\Messenger\\\\SendEmailMessage\\":2:{s:60:\\"\\0Symfony\\\\Component\\\\Mailer\\\\Messenger\\\\SendEmailMessage\\0message\\";O:39:\\"Symfony\\\\Bridge\\\\Twig\\\\Mime\\\\TemplatedEmail\\":4:{i:0;s:41:\\"registration/confirmation_email.html.twig\\";i:1;N;i:2;a:3:{s:9:\\"signedUrl\\";s:166:\\"https://127.0.0.1:8000/verify/email?expires=1683877651&signature=gvoGrh8gcaC2faZb%2BDXFUmlSLLXJo0oKeKpK5afGDS4%3D&token=WX90jwdgEKsR1FAIDo4Z5fVY9Jn50d030vlkBtjKvU4%3D\\";s:19:\\"expiresAtMessageKey\\";s:26:\\"%count% hour|%count% hours\\";s:20:\\"expiresAtMessageData\\";a:1:{s:7:\\"%count%\\";i:1;}}i:3;a:6:{i:0;N;i:1;N;i:2;N;i:3;N;i:4;a:0:{}i:5;a:2:{i:0;O:37:\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\Headers\\":2:{s:46:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\Headers\\0headers\\";a:3:{s:4:\\"from\\";a:1:{i:0;O:47:\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\":5:{s:50:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0name\\";s:4:\\"From\\";s:56:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lineLength\\";i:76;s:50:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lang\\";N;s:53:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0charset\\";s:5:\\"utf-8\\";s:58:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\0addresses\\";a:1:{i:0;O:30:\\"Symfony\\\\Component\\\\Mime\\\\Address\\":2:{s:39:\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0address\\";s:19:\\"skatespot@admin.com\\";s:36:\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0name\\";s:25:\\"Seb (admin de Skate Spot)\\";}}}}s:2:\\"to\\";a:1:{i:0;O:47:\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\":5:{s:50:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0name\\";s:2:\\"To\\";s:56:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lineLength\\";i:76;s:50:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lang\\";N;s:53:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0charset\\";s:5:\\"utf-8\\";s:58:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\MailboxListHeader\\0addresses\\";a:1:{i:0;O:30:\\"Symfony\\\\Component\\\\Mime\\\\Address\\":2:{s:39:\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0address\\";s:15:\\"admin@admin.com\\";s:36:\\"\\0Symfony\\\\Component\\\\Mime\\\\Address\\0name\\";s:0:\\"\\";}}}}s:7:\\"subject\\";a:1:{i:0;O:48:\\"Symfony\\\\Component\\\\Mime\\\\Header\\\\UnstructuredHeader\\":5:{s:50:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0name\\";s:7:\\"Subject\\";s:56:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lineLength\\";i:76;s:50:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0lang\\";N;s:53:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\AbstractHeader\\0charset\\";s:5:\\"utf-8\\";s:55:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\UnstructuredHeader\\0value\\";s:25:\\"Please Confirm your Email\\";}}}s:49:\\"\\0Symfony\\\\Component\\\\Mime\\\\Header\\\\Headers\\0lineLength\\";i:76;}i:1;N;}}}s:61:\\"\\0Symfony\\\\Component\\\\Mailer\\\\Messenger\\\\SendEmailMessage\\0envelope\\";N;}}', '[]', 'default', '2023-05-12 06:47:32', '2023-05-12 06:47:32', NULL);
 
 -- Listage de la structure de table skatespot. module
 CREATE TABLE IF NOT EXISTS `module` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table skatespot.module : ~5 rows (environ)
+-- Listage des données de la table skatespot.module : ~3 rows (environ)
 INSERT INTO `module` (`id`, `name`) VALUES
-	(1, 'curb'),
-	(2, 'plan incliné'),
-	(3, 'table'),
-	(4, 'rail'),
-	(5, 'gap');
+	(1, 'rail'),
+	(2, 'curb'),
+	(3, 'plan incliné');
 
 -- Listage de la structure de table skatespot. spot
 CREATE TABLE IF NOT EXISTS `spot` (
@@ -92,17 +90,20 @@ CREATE TABLE IF NOT EXISTS `spot` (
   `city` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `lat` double NOT NULL,
   `lng` double NOT NULL,
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_validated` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `IDX_B9327A73F675F31B` (`author_id`),
   CONSTRAINT `FK_B9327A73F675F31B` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table skatespot.spot : ~4 rows (environ)
-INSERT INTO `spot` (`id`, `author_id`, `name`, `description`, `adress`, `cp`, `city`, `lat`, `lng`) VALUES
-	(1, 1, 'Le zems', 'Parvis du musée d\'art contemporain de la ville de Strasbourg. La pratique du skate y est autorisé par le musée et il y a une forte fréquentation.', NULL, NULL, NULL, 48.579739, 7.737239),
-	(2, 1, 'Le tribu', 'Parvis du tribunal de la ville de Strasbourg. Dalles très lisses, veuillez a respecter les lieux', NULL, NULL, NULL, 48.58802, 7.747644),
-	(3, 1, 'TestFORM5', 'aaaa', NULL, NULL, NULL, 49.5, 7.6),
-	(4, 1, 'TestBDD', 'azrezrar', NULL, NULL, NULL, 48, 7);
+-- Listage des données de la table skatespot.spot : ~5 rows (environ)
+INSERT INTO `spot` (`id`, `author_id`, `name`, `description`, `adress`, `cp`, `city`, `lat`, `lng`, `creation_date`, `is_validated`) VALUES
+	(4, NULL, 'Nom de spot 3', 'azrzer', NULL, NULL, NULL, 48.569908, 7.737421, '2023-05-12 08:00:14', 1),
+	(5, 1, 'Nom de spot 4', 'blabla', NULL, NULL, NULL, 48.576, 7.76, '2023-05-12 12:20:05', 1),
+	(6, 1, 'Nom de spot 0', 'blablaaa', NULL, NULL, NULL, 48.586, 7.771, '2023-05-12 12:28:47', 0),
+	(8, 1, 'Nom de spot 6', 'zerldf', NULL, NULL, NULL, 48.58856, 7.754116, '2023-05-12 12:27:57', 1),
+	(10, 1, 'Nom de spot 1', 'ziiziziazeae', NULL, NULL, NULL, 48.584, 7.749, '2023-05-12 13:04:47', 1);
 
 -- Listage de la structure de table skatespot. spot_module
 CREATE TABLE IF NOT EXISTS `spot_module` (
@@ -115,12 +116,9 @@ CREATE TABLE IF NOT EXISTS `spot_module` (
   CONSTRAINT `FK_39B5CF4CAFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Listage des données de la table skatespot.spot_module : ~4 rows (environ)
+-- Listage des données de la table skatespot.spot_module : ~1 rows (environ)
 INSERT INTO `spot_module` (`spot_id`, `module_id`) VALUES
-	(1, 1),
-	(1, 2),
-	(1, 5),
-	(2, 1);
+	(10, 3);
 
 -- Listage de la structure de table skatespot. user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -130,14 +128,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `is_verified` tinyint(1) NOT NULL,
   `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `registration_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `registration_date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table skatespot.user : ~1 rows (environ)
 INSERT INTO `user` (`id`, `email`, `roles`, `password`, `is_verified`, `pseudo`, `registration_date`) VALUES
-	(1, 'exemple@exemple.com', '[]', '123', 1, 'exemplePseudo', '2023-05-10 09:18:54');
+	(1, 'admin@admin.com', '["ROLE_ADMIN"]', '$2y$13$Ng/c4aoXNX0od2mEktVMDeBtbgq5obBfaze35dOg2ydNy7dnPXx7q', 0, 'my_admin', '2023-05-12 06:47:31');
 
 -- Listage de la structure de table skatespot. user_spot
 CREATE TABLE IF NOT EXISTS `user_spot` (
