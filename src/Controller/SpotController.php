d5006e447b0414cf661c54947ab6cd6a5aa14f67
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2023-09-07 16:57:02 +0200

    securing roots and fixed mailHog verif system

--- src/Controller/SpotController.php
+++ src/Controller/SpotController.php
@@ -39,13 +39,16 @@ class SpotController extends AbstractController
     )]
     public function index(Security $security, ManagerRegistry $doctrine, Spot $spot = null, Request $request, PictureService $pictureService, PaginatorInterface $paginator, SpotRepository $spotRepository): Response
     {
+        // Définition des variables de base****************************************************************************************************************
         //on accède aux méthodes du manager de doctrine
         $entityManager = $doctrine->getManager();
 
+        //Définition du User
+        $user = $security->getUser();
         //récupère tout les spots de la BDD pour les marqueurs sur la carte
         $spots = $doctrine->getRepository(Spot::class)->findBy([], ['name' => 'ASC']);
 
-        // Requête pour la pagination des spots
+        // Requête pour la pagination des spots***********************************************************************************************************************
         $spotsQuery = $doctrine->getRepository(Spot::class)
             ->createQueryBuilder('s')
             ->orderBy('s.name', 'ASC')
@@ -57,16 +60,13 @@ class SpotController extends AbstractController
             5 // Nombre d'elements par page
         );
 
-        //Définition du User
-        $user = $security->getUser();
-
-// Gestion des FILTRES *********************************************//
+        // Gestion des FILTRES ****************************************************************************************************************************
         // instancie un nouvel object SearchData
         $searchData = new SearchData();
         // création du formulaire SpotSearchType pour les filtres
         $formSearch = $this->createForm(SpotSearchType::class, $searchData);
         // interception du formulaire SpotSearchType lorsqu'une requete est fournie 
-        $formSearch->handleRequest($request);  
+        $formSearch->handleRequest($request);
         // instancie une variable $sportFiltered 
         $spotsFiltered = $spots; // égale a $spots tant qu'aucun filtre n'as été renseigner.
         $filtersEmpty = false;
@@ -80,16 +80,19 @@ class SpotController extends AbstractController
             $coveredFilter = $searchData->covered;
             $orderCreation = $searchData->orderCreation;
             $spotsFiltered = $spotRepository->findByCriteria($searchFilter, $moduleFilter, $officialFilter, $coveredFilter, $orderCreation);
-            if(empty($spotsFiltered)) {
+            if (empty($spotsFiltered)) {
                 $filtersEmpty = true;
             }
         }
-        /*Créer un tableau $tab
-        /Construit un tableau associatif contenant le nom du spot comme clé.
-        /Chaque clé est associée a un tableau contenant sa latitude, sa longitude, 
-        /sa description et son état de validation comme valeur*/
-        $tab = [];
+        // Récupération des spots pour créé les MARKERS ************************************************************************************************
+        /*
+        Construit un tableau associatif contenant le nom du spot comme clé.
+        Chaque clé est associée a un tableau contenant sa latitude, sa longitude, 
+        sa description et son état de validation comme valeur
+        */
 
+        // instancie un tableau $tab
+        $tab = [];
         // Si des filtres ont été selectionné, $spotsFiltered n'est pas empty
         // dans ce cas,j'utilise $spotsFiltered pour créé les marqueurs
         if (!empty($spotsFiltered)) {
@@ -108,7 +111,7 @@ class SpotController extends AbstractController
                     ]
                 ];
             }
-            // Sinon, pas de filtres. J'utilise $spots pour créé les marqueurs (TOUS les spots de ma BDD)
+            // Sinon, pas de filtres. J'utilise $spots pour créé les marqueurs (avec TOUS les spots de ma BDD)
         } else {
             foreach ($spots as $aspot) {
                 $tab[] = [
@@ -134,20 +137,23 @@ class SpotController extends AbstractController
         $tabCoords = json_encode($tab, JSON_HEX_APOS);
 
 
-        /**********************AJOUT DE SPOT*********************************** */
-        //récupérer la liste de tout les modules
-        $modules = $doctrine->getRepository(Module::class)->findAll();
+    /**********************AJOUT DE SPOT*********************************** */
+    
+    //récupérer la liste de tout les modules
+    $modules = $doctrine->getRepository(Module::class)->findAll();
+    //créé un formulaire qui se repose sur le builder (qui se repose lui mm sur les propriétés de la classe) et assigner la liste de tout les modules pour les checkboxs
+    $form = $this->createForm(SpotType::class, $spot, [
+        'modules' => $modules
+    ]);
 
-        //créé un formulaire qui se repose sur le builder (qui se repose lui mm sur les propriétés de la classe) et assigner la liste de tout les modules pour les checkboxs
-        $form = $this->createForm(SpotType::class, $spot, [
-            'modules' => $modules
-        ]);
+    // Vérifier si le client est connecté avec un compte
+    if($user){
         //lorsqu'une requete est soumise, récupère les données
         $form->handleRequest($request);
-
         //assigne les donnée du formulaire soumis à une variable 
         $newspot = $form->getData();
 
+        // Pour vérifier si le spot existe déjà (modification) ou si il est nouveau (création)
         //défini isNew comme null
         $isNew = null;
         //si mon spot n'existe pas, créé un nouveau spot.
@@ -157,7 +163,7 @@ class SpotController extends AbstractController
             $isNew = true;
         }
 
-        if ($form->isSubmitted() && $form->isValid() && $user) {
+        if ($form->isSubmitted() && $form->isValid()) {
             // assigne les donnée du formulaire soumis à une variable 
             $newspot = $form->getData();
 
@@ -201,6 +207,7 @@ class SpotController extends AbstractController
             //refresh la page
             return $this->redirectToRoute('app_spot');
         }
+    }
         /********************** FIN AJOUT DE SPOT*********************************** */
 
         //retourne la réponse http affichée par le navigateur.
@@ -270,112 +277,114 @@ class SpotController extends AbstractController
             //créé un formulaire qui se repose sur le builder
             $formPicture = $this->createForm(PictureType::class);
 
-            /* Pour le formulaire de Comment */
-            //intercepte la requete du formulaire soumis
-            $formComment->handleRequest($request);
-
-            //assigne les donnée du formulaire soumis à une variable 
-            $newComment = $formComment->getData();
-
-            //défini isNew comme null (pour CREER)
-            $isNew = false;
-            //si mon comment n'existe pas, créé un nouveau commentaire.
-            if (!$comment) {
-                $comment = new Comment();
-                $isNew = true;
-            }
-
-            //Si le formulaire est soumis ET valide
-            if ($formComment->isSubmitted() && $formComment->isValid()) {
+            // et que le client est connecté
+            if($user){
+                /* Pour le formulaire de Comment */
+                //intercepte la requete du formulaire soumis
+                $formComment->handleRequest($request);
 
                 //assigne les donnée du formulaire soumis à une variable 
                 $newComment = $formComment->getData();
 
-                //défini quel spot est concerné par le commentaire
-                $newComment->setSpotConcerned($spot);
-
-
-                //Si c'est un nouveau commentaire, assigne la date et l'autheur
-                if ($isNew) {
-                    // Pour définir la date/heure actuelle comme date du commentaire
-                    $now = new \DateTime();
-                    $newComment->setDate($now);
-
-                    //Pour définir l'author du comm comme étant le user qui soumet le formulaire
-                    $user = $security->getUser();
-                    $newComment->setAuthor($user);
+                //défini isNew comme null (pour CREER)
+                $isNew = false;
+                //si mon comment n'existe pas, créé un nouveau commentaire.
+                if (!$comment) {
+                    $comment = new Comment();
+                    $isNew = true;
                 }
 
-                //prepare la requette
-                $entityManager->persist($newComment);
-                //execute pour ajouter le comm en BDD
-                $entityManager->flush();
-                //refresh la page
-                return $this->redirectToRoute('show_spot', ["idSpot" => $spot->getId()]);
-            }
-            /* Pour le formulaire de Notation */
-            $formNotation->handleRequest($requestNotation);
-            // Vérifier si le User a déjà noté ce Spot
-            $existingNotation = $doctrine->getRepository(Notation::class)->findOneBy([
-                'spot' => $spot,
-                'user' => $user
-            ]);
-            // Si une notation existante est trouvée pour l'utilisateur et le spot, utilisez-la, sinon créez une nouvelle instance de Notation
-            $notationToSave = $existingNotation ? $existingNotation : new Notation();
+                //Si le formulaire est soumis ET valide
+                if ($formComment->isSubmitted() && $formComment->isValid()) {
 
-            //Si le formulaire de NOTATION est soumis et valide
-            if ($formNotation->isSubmitted() && $formNotation->isValid()) {
+                    //assigne les donnée du formulaire soumis à une variable 
+                    $newComment = $formComment->getData();
 
-                if ($existingNotation) {
-                    // L'utilisateur a déjà noté ce spot: mise à jour de la notation
-                    $this->addFlash('info', 'Votre notation a été mise à jour.');
-                } else {
-                    // Ajout d'une nouvelle notation
-                    $notationToSave->setSpot($spot);
-                    $notationToSave->setUser($user);
+                    //défini quel spot est concerné par le commentaire
+                    $newComment->setSpotConcerned($spot);
+
+
+                    //Si c'est un nouveau commentaire, assigne la date et l'autheur
+                    if ($isNew) {
+                        // Pour définir la date/heure actuelle comme date du commentaire
+                        $now = new \DateTime();
+                        $newComment->setDate($now);
+
+                        //Pour définir l'author du comm comme étant le user qui soumet le formulaire
+                        $user = $security->getUser();
+                        $newComment->setAuthor($user);
+                    }
+
+                    //prepare la requette
+                    $entityManager->persist($newComment);
+                    //execute pour ajouter le comm en BDD
+                    $entityManager->flush();
+                    //refresh la page
+                    return $this->redirectToRoute('show_spot', ["idSpot" => $spot->getId()]);
+                }
+                /* Pour le formulaire de Notation */
+                $formNotation->handleRequest($requestNotation);
+                // Vérifier si le User a déjà noté ce Spot
+                $existingNotation = $doctrine->getRepository(Notation::class)->findOneBy([
+                    'spot' => $spot,
+                    'user' => $user
+                ]);
+                // Si une notation existante est trouvée pour l'utilisateur et le spot, utilisez-la, sinon créez une nouvelle instance de Notation
+                $notationToSave = $existingNotation ? $existingNotation : new Notation();
+
+                //Si le formulaire de NOTATION est soumis et valide
+                if ($formNotation->isSubmitted() && $formNotation->isValid()) {
+
+                    if ($existingNotation) {
+                        // L'utilisateur a déjà noté ce spot: mise à jour de la notation
+                        $this->addFlash('info', 'Votre notation a été mise à jour.');
+                    } else {
+                        // Ajout d'une nouvelle notation
+                        $notationToSave->setSpot($spot);
+                        $notationToSave->setUser($user);
+                    }
+
+                    $notationToSave->setNote($formNotation->get('note')->getData()); // Ajoutez ceci si 'note' est un champ de votre formulaire. Ajustez en fonction des champs réels.
+
+                    // Prépare la requête
+                    $entityManager->persist($notationToSave);
+
+                    // Exécute pour ajouter/actualiser la notation en BDD
+                    $entityManager->flush();
+
+                    return $this->redirectToRoute('show_spot', ["idSpot" => $spot->getId()]);
                 }
 
-                $notationToSave->setNote($formNotation->get('note')->getData()); // Ajoutez ceci si 'note' est un champ de votre formulaire. Ajustez en fonction des champs réels.
+                /*********** GESTION DES IMAGES ******/
+                // On intercepte le formulaire d'image soumis
+                $formPicture->handleRequest($request);
+                //Si le formulaire de PICTURE est soumis et valide
+                if ($formPicture->isSubmitted() && $formPicture->isValid()) {
+                    // On récupère les images
+                    $images = $formPicture->get('pictures')->getData();
 
-                // Prépare la requête
-                $entityManager->persist($notationToSave);
-                
-                // Exécute pour ajouter/actualiser la notation en BDD
-                $entityManager->flush();
+                    foreach ($images as $image) {
+                        // On défini le dossier de destination
+                        $folder = 'photos-spot';
+                        // On appel le service d'ajout
+                        $fichier = $pictureService->add($image, $folder, 300, 300);
+                        // On instancie un nouvel objet image
+                        $img = new Picture();
+                        // On lui assigne un nom (renvoyé par le service) et un spot (défini via l'ID dans l'url ?)
+                        $img->setName($fichier);
+                        $img->setSpot($spot);
 
-                return $this->redirectToRoute('show_spot', ["idSpot" => $spot->getId()]);
-            }
+                        // On ajoute les images au spot
+                        $spot->addPicture($img);
+                    }
+                    //prepare la requette
+                    $entityManager->persist($img);
+                    //execute pour ajouter la picture en BDD
+                    $entityManager->flush();
 
-/*********** GESTION DES IMAGES ******/
-            // On intercepte le formulaire d'image soumis
-            $formPicture->handleRequest($request);
-            //Si le formulaire de PICTURE est soumis et valide
-            if ($formPicture->isSubmitted() && $formPicture->isValid()) {
-                // On récupère les images
-                $images = $formPicture->get('pictures')->getData();
-
-                foreach ($images as $image) {
-                    // On défini le dossier de destination
-                    $folder = 'photos-spot';
-                    // On appel le service d'ajout
-                    $fichier = $pictureService->add($image, $folder, 300, 300);
-                    // On instancie un nouvel objet image
-                    $img = new Picture();
-                    // On lui assigne un nom (renvoyé par le service) et un spot (défini via l'ID dans l'url ?)
-                    $img->setName($fichier);
-                    $img->setSpot($spot);
-
-                    // On ajoute les images au spot
-                    $spot->addPicture($img);
+                    return $this->redirectToRoute('show_spot', ["idSpot" => $spot->getId()]);
                 }
-                //prepare la requette
-                $entityManager->persist($img);
-                //execute pour ajouter la picture en BDD
-                $entityManager->flush();
-
-                return $this->redirectToRoute('show_spot', ["idSpot" => $spot->getId()]);
             }
-
             return $this->render('spot/show.html.twig', [
                 'controller_name' => 'SpotController',
                 'spot' => $spot,
@@ -386,7 +395,7 @@ class SpotController extends AbstractController
             ]);
         }
         // Si le spot n'existe pas, redirection vers la map
-        else{
+        else {
             return $this->redirectToRoute('app_spot');
         }
     }
