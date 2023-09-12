528aaf6b3ad67a11a37c5cf948448f018d956bca
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2023-09-12 16:36:04 +0200

    toggle element filter container

---
+++ public/js/filters-listing.js
@@ -0,0 +1,25 @@
+const toggleFilters = document.getElementById('toggleFilters')
+
+toggleFilters.addEventListener('click', function() {
+    const container = document.getElementById('search-form');
+    const arrows = document.querySelectorAll('.arrow-toggle-filters');
+
+    if (container.style.maxHeight !== '0px') {
+        container.style.maxHeight = '0px';
+        container.style.overflow = 'hidden';
+        toggleFilters.style.top = '92px' 
+        arrows.forEach(function(arrow) {
+            arrow.style.rotate = '180deg';
+            // Do something with each arrow element
+        });
+        
+    } else {
+        toggleFilters.style.top = '474px' 
+        container.style.maxHeight = '100%'; // Rétablissez ceci à votre max-width initial*
+        arrows.forEach(function(arrow) {
+            // Do something with each arrow element
+            arrow.style.rotate = 'unset';
+        });
+        
+    }
+});
