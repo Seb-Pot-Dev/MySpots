66fbeedc129bdba84e361d02a656ccfd47318331
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2023-10-04 20:30:33 +0200

    correct syntax

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -23,7 +23,7 @@ class AdminController extends AbstractController
     {
         // définition du $user actif
         $user=$this->getUser();
-        $userRole = $user->getRole();
+        $userRole = $user->getRoles();
         // Si le $user possède le role admin
         if ($user && in_array('ROLE_ADMIN', $userRole)) {
                 if ($spot){
